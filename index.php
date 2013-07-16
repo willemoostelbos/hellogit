4f72c0c8a093314a7bc1dc0c82f6d14bd12c1cf1
Willem Oostelbos <willem@e-guerrillas.nl>
2013-07-16 10:17:02 +0200

    base cleaning files

---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+
+</head>
+
+<body >
+
+<div style="height:100%; text-align:center;vertical-align:middle;">
+<img src=yapimasamasinda.gif>
+
+<br><br>
+
+<h2>Under Construction</h2><br>
+This site is hosted on an EHCP server, <a href=http://www.ehcp.net>www.ehcp.net</a>, ehcp is a hosting control panel for ubuntu, <br>
+To change this, Upload your files inside httpdocs folder using ftp.<br>
+<br>
+
+<script type="text/javascript"><!--
+google_ad_client = "pub-0768633782379013";
+google_ad_width = 468;
+google_ad_height = 60;
+google_ad_format = "468x60_as";
+google_ad_type = "text_image";
+google_ad_channel = "";
+//-->
+</script>
+<script type="text/javascript"
+  src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+</div>
+
+</body>
+</html>
+
+
